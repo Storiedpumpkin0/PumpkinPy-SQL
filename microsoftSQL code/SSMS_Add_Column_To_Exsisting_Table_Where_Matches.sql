4b65407f0c6b1543093a054ed7cce36b763ccef4
Storiedpumpkin0 <54968436+Storiedpumpkin0@users.noreply.github.com>
2020-06-24 15:39:28 -0400

    Update SSMS_Add_Column_To_Exsisting_Table_Where_Matches.sql

--- microsoftSQL code/SSMS_Add_Column_To_Exsisting_Table_Where_Matches.sql
+++ microsoftSQL code/SSMS_Add_Column_To_Exsisting_Table_Where_Matches.sql
@@ -13,13 +13,13 @@
 WARNING ! ! ! ! WARNING! ! ! ! ! WARNING ! ! ! ! WARNING! ! ! ! ! WARNING ! ! ! ! WARNING! ! ! ! ! WARNING ! ! ! ! WARNING! ! ! ! ! WARNING 
 	
 					-- This script will DROP anything at :
+							test.dbo.origin
 							test.dbo.temp
 							test.dbo.csv_temp
  
 ----------------------------------------------------------------------------------------------------------------------------------------*/ 
 
 -- create a csv_temp table
-
 CREATE TABLE test.dbo.csv_temp(	
 	ID varchar(50), 
 	new_column varchar(50)   
@@ -34,7 +34,7 @@ WITH(
 	)
 
 --create temp table to join data from the two tables
-DROP TABLE test.dbo.temp
+--DROP TABLE test.dbo.temp /* <--- This is here in case the temp table already exsists
 CREATE TABLE test.dbo.temp(
 	ID	varchar(10),
 	old_column1	varchar(10),
@@ -44,9 +44,22 @@ CREATE TABLE test.dbo.temp(
 
 -- combine the data from the csv_temp table and the origin 
 INSERT INTO test.dbo.temp (ID, old_column1, old_column2, new_column)
-	SELECT DISTINCT 
+	SELECT DISTINCT o.ID, o.column1, o.column2, c.new_column
 	FROM	test.dbo.csv_temp c
-	JOIN	test.dbo.orin o on o.ID = c.ID 
+	JOIN	test.dbo.orign o on o.ID = c.ID 
 
+--Here you could drop the old origin table and rebuild it using the temp table like this:
+DROP TABLE test.dbo.origin
+CREATE TABLE test.dbo.origin (
+	ID			varchar(10) NOT NULL, 
+	column1		varchar(10) NOT NULL,
+	column2		varchar(10)	NOT NULL, 
+	new_column	varchar(10)	NOT NULL
+	)
+INSERT INTO test.dbo.origin (ID, column1, column2, new_column)
+	SELECT ID, old_column1, old_column2, new_column
+	FROM test.dbo.temp
+	
 -- do some cleanup 
 DROP TABLE test.dbo.csv_temp
+DROP TABLE test.dbo.temp
